349f69caf6033f7f70d33cc633d80de63c20db14
snehalXcode <snehalsathawane2511@gmail.com>
2026-06-23 19:26:47 +0530

    binary search on answers template

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,24 @@
+int floorSqrt(int n) {
+
+//     int low = 1;
+//     int high = n;
+
+//     int ans = 0;
+
+//     while(low <= high) {
+
+//         int mid = low + (high - low) / 2;
+
+//         if(1LL * mid * mid <= n) {
+
+//             ans = mid;
+//             low = mid + 1;
+//         }
+//         else {
+
+//             high = mid - 1;
+//         }
+//     }
+
+//     return ans;
+// }
